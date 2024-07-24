ec0ef0edd5efcabf79d84113f5c0f81c578765d8
watanabe121 <80362216+watanabe121@users.noreply.github.com>
2024-07-24 23:25:45 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,18 +4,20 @@ function getRandomInt(max) {
 
 let num = getRandomInt(1000);
 
-if (num % 3 === 0) {
-    console.log('3の倍数です');
+console.log(num);
+
+if ( num % 3 === 0 && num % 5 === 0) {
+    console.log('３と５の倍数です');
 }
 
 else if (num % 5 === 0) {
     console.log('5の倍数です');
 }
 
-else if ( num % 3 === 0 || num % 5 === 0) {
-    console.log('３と５の倍数です')
+else if (num % 3 === 0) {
+    console.log('3の倍数です');
 }
 
 else {
-    console.log(num)
+    console.log(num);
 }
