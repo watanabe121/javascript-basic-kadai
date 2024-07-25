be86cd9578fe233e394fac5517b1a7568970b203
watanabe121 <80362216+watanabe121@users.noreply.github.com>
2024-07-25 23:10:53 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,11 @@
+const holiDays = ['正月', '成人の日', '建国記念日', '天皇誕生日', '春分の日', '昭和の日', '憲法記念日', 'みどりの日', 'こどもの日', '海の日', '山の日', '敬老の日', '秋分の日', 'スポーツの日', '文化の日', '勤労感謝の日']
+
+for (let i = 0; i < holiDays.length; i++) {
+    console.log(holiDays[i]);
+}
+
+let i = 0;
+while (i < holiDays.length) {
+    console.log(holiDays[i]);
+    i++;
+}
