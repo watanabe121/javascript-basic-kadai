395a51fec781619612b8e229b450dcbf3d95a59c
watanabe121 <80362216+watanabe121@users.noreply.github.com>
2024-07-27 17:50:54 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const date = new Date("2024-07-27");
+const [month, day, year] = [
+    date.getMonth(),
+    date.getDate(),
+    date.getFullYear(),
+];
+console.log(year + '年' + month + '月' + day + '日');
