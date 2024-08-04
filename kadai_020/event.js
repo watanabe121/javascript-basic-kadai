f8cba9fb28a7cad0cf975c36915fb27dd8ab5377
watanabe121 <80362216+watanabe121@users.noreply.github.com>
2024-08-04 14:24:51 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const addBtn = document.getElementById('btn');
+
+const parentText = document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+    const childText = document.createElement('li');
+    text.textContent = 'ボタンをクリックしました';
+});
